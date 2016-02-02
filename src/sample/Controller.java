6da2b3838eec0dffd426e81f6f5e9e200e225b21
Lise <s305345@stud.hioa.no>
2016-02-02 15:44:35 +0100

    Fixed FXML so the Controller works.  Added some code to draw a square.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,23 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 public class Controller {
-    @FXML
-    private Canvas theCanvas;
+    @FXML private Canvas theCanvas;
+    public void initialize(){
+        System.out.println("Hallo!");
+        GraphicsContext gc = theCanvas.getGraphicsContext2D();
+        draw(gc);
 
+    }
+
+    private void draw(GraphicsContext gc) {
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(2);
+        gc.strokeRect(1, 1, 100, 100);
+
+    }
 
 }
