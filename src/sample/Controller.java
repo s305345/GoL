dddcf192e8bf0730439900d0014d7261243da288
Lise <s305345@stud.hioa.no>
2016-02-02 16:39:18 +0100

    fixed the order in fxml

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,16 +8,18 @@ import javafx.scene.paint.Color;
 public class Controller {
     @FXML private Canvas theCanvas;
     public void initialize(){
-        System.out.println("Hallo!");
         GraphicsContext gc = theCanvas.getGraphicsContext2D();
         draw(gc);
+
+
+        boolean cells[][] = new boolean[200][300];
 
     }
 
     private void draw(GraphicsContext gc) {
         gc.setStroke(Color.BLACK);
-        gc.setLineWidth(2);
-        gc.strokeRect(1, 1, 100, 100);
+        gc.setLineWidth(1);
+        gc.strokeRect(1, 1, 20, 20);
 
     }
 
